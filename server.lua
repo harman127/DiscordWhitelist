c3bcb572a37f0ff9d894f71eb9df3bd4bc79384b
harman127 <37600124+harman127@users.noreply.github.com>
2020-02-04 12:40:36 -0500

    add debug logging for client
    added debug function, allowed the system to check more than just the first role before ending deferrals.

--- server.lua
+++ server.lua
@@ -5,6 +5,8 @@
 --- Config ---
 notWhitelisted = "You are not whitelisted for this server." -- Message displayed when they are not whitelist with the role
 noDiscord = "You must have Discord open to join this server." -- Message displayed when discord is not found
+debug = false
+
 
 roles = { -- Role nickname(s) needed to pass the whitelist
     "Role1",
@@ -16,6 +18,7 @@ roles = { -- Role nickname(s) needed to pass the whitelist
 --- Code ---
 
 AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
+    local role_found = false
     local src = source
     deferrals.defer()
     deferrals.update("Checking Permissions")
@@ -23,16 +26,25 @@ AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
     for k, v in ipairs(GetPlayerIdentifiers(src)) do
         if string.sub(v, 1, string.len("discord:")) == "discord:" then
             identifierDiscord = v
+            if debug then
+                deferrals.update("Discord ID found. Requesting check with discord_perms.")
+            end
         end
     end
 
     if identifierDiscord then
         for i = 1, #roles do
-            if exports.discord_perms:IsRolePresent(src, roles[i]) then
-                deferrals.done()
-            else
-                deferrals.done(notWhitelisted)
+            if debug then
+                deferrals.update("Checking to see if user has "..roles[i])
             end
+            if exports.discord_perms:IsRolePresent(src, roles[i]) then
+                role_found = true
+            end
+        end
+        if role_found then
+            deferrals.done()
+        else
+            deferrals.done(notWhitelisted)
         end
     else
         deferrals.done(noDiscord)
